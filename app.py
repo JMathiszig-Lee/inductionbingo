e8aa8ffa33948b2128f4ee2ac1a84ded15dfa238
JMathiszig-Lee <jakob@mathisziglee.co.uk>
2018-07-04 11:37:46 +0100

    flask not running

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import connexion
+
 from connexion.resolver import RestyResolver
+from flask import Flask
 
-
+app=Flask(__name__)
 
 if __name__ == '__main__':
     app = connexion.App(__name__, 9090, specification_dir='swagger/')
     app.add_api('api.yaml', resolver=RestyResolver('api'))
-    app.run()
+    app.run(debug=True)
