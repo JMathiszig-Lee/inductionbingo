b226200014f48537f665eaaf1e6088f5ad45bd17
JMathiszig-Lee <jakob@mathisziglee.co.uk>
2018-07-04 11:08:34 +0100

    first commit, following tutorial

---
+++ api/items.py
@@ -0,0 +1,8 @@
+items = {
+    0: {"name": "First item"},
+    1: {"name": "second item"}
+}
+
+
+def search() -> list:
+    return items
---
+++ app.py
@@ -0,0 +1,9 @@
+import connexion
+from connexion.resolver import RestyResolver
+
+
+
+if __name__ == '__main__':
+    app = connexion.App(__name__, 9090, specification_dir='swagger/')
+    app.add_api('api.yaml', resolver=RestyResolver('api'))
+    app.run()
