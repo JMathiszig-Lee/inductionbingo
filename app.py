e86d95a7be0abb6fa4a6304ba2fb29a141ba31a6
JMathiszig-Lee <jakob@mathisziglee.co.uk>
2018-07-04 15:59:29 +0100

    battery status works, trying to get it to post to API

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from connexion.resolver import RestyResolver
+from flask import Flask
+from flask import render_template
 import connexion
 
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route("/test")
+def test():
+    return render_template('test.html')
+
 if __name__ == '__main__':
-    app = connexion.App(__name__, specification_dir='swagger/')
+    app = connexion.FlaskApp(__name__, specification_dir='swagger/')
     app.add_api('api.yaml', resolver=RestyResolver('api'))
     app.run(host='127.0.0.1', port=9090)
